3858015280307b87b6d00f43204f5468c06857d4
027DeepeekaChudasama <deepeekachudasama@gmail.com>
2022-03-08 16:32:27 +0530

    updated

---
+++ 1B/Deepeeka_Milestone1B.py
@@ -0,0 +1,101 @@
+import yaml
+import sys
+from datetime import datetime,timedelta
+import time
+from time import sleep, perf_counter
+from threading import Thread
+
+def function_1(activities, key):
+    if activities['Execution'] == 'Sequential':
+        function_2(activities['Activities'], key)
+    elif activities['Execution'] == 'Concurrent':
+        function_3(activities['Activities'], key)
+    
+
+def function_2(activities, key):
+    for k in activities:
+        if activities[k]['Type'] == 'Task':
+            print(datetime.now(), end=';')
+            print(f'{key}.{k} Entry')
+            if activities[k]['Function'] == 'TimeFunction':
+                input =activities[k]['Inputs']['FunctionInput']
+                process_time = activities[k]['Inputs']['ExecutionTime']
+                print(datetime.now(), end=';')
+                print(f'{key}.{k} Executing TimeFunction ({input}, {process_time})')
+                time.sleep(int(process_time))
+
+            print(datetime.now(), end=';')
+            print(f'{key}.{k} Exit')
+
+        elif activities[k]['Type'] == 'Flow':
+            print(datetime.now(), end=';')
+            print(f'{key}.{k} Entry')
+            function_1(activities[k], key+'.'+k)
+            print(datetime.now(), end=';')
+            print(f'{key}.{k} Exit')
+
+
+def function_3(activities, key):
+    threads = []
+    a = []
+    for k in activities:
+        if activities[k]['Type'] == 'Task':
+            t = Thread(target=function_4, args=(activities,key,k))
+            threads.append(t)
+            t.start()
+            string="{}.{}".format(key,k)
+            a.append(string)
+            continue
+
+        elif activities[k]['Type'] == 'Flow':
+            t = Thread(target=function_5, args=(activities,key,k))
+            threads.append(t)
+            t.start()
+            string="{}.{}".format(key,k)
+            a.append(string)
+            continue
+        
+    for t in threads:
+        t.join()
+    
+    for j in (len(a)-1, -1, -1):      
+        print(datetime.now(), end=';')
+        print(f'{a[j]} Exit')      
+        
+            
+    # print(datetime.now(), end=';')
+    # print(f'{key} Exit')
+    
+
+
+def function_4(activities,key,k):
+    print(datetime.now(), end=';')
+    print(f'{key}.{k} Entry')
+    if activities[k]['Function'] == 'TimeFunction':
+        input =activities[k]['Inputs']['FunctionInput']
+        process_time = activities[k]['Inputs']['ExecutionTime']
+        print(datetime.now(), end=';')
+        print(f'{key}.{k} Executing TimeFunction ({input}, {process_time})')
+        time.sleep(int(process_time))
+        
+def function_5(activities,key,k):
+    print(datetime.now(), end=';')
+    print(f'{key}.{k} Entry')
+    function_1(activities[k], key+'.'+k)
+
+
+with open('Milestone1B.yaml', 'r') as file:
+    my_dict = yaml.load(file, Loader=yaml.FullLoader)
+
+stdoutOrigin = sys.stdout
+sys.stdout = open("log.txt", "w")
+
+key = list(my_dict.keys())[0]
+print(datetime.now(), end=';')
+print(f'{key} Entry')
+function_1(my_dict[key], key)
+print(datetime.now(), end=';')
+print(f'{key} Exit')
+
+sys.stdout.close()
+sys.stdout = stdoutOrigin
